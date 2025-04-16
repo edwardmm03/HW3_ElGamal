94bd20f00ab082d57c9cbbd14d69cf890b58a0cc
Bench <michael.a.bencivenga@gmail.com>
2025-04-16 10:52:03 -0400

    changed is_prime function to be miller-rabin primality test, so much faster, tested with bit lengths of up to 1024 and still very fast

--- rsa.py
+++ rsa.py
@@ -1,15 +1,27 @@
 import random
 import math
 
-def is_prime(n):
-    if n < 2:
-        return False
-    if n == 2:
+def is_prime(n, k=10):
+    if n == 2 or n == 3:
         return True
-    if n % 2 == 0:
+    if n <= 1 or n % 2 == 0:
         return False
-    for i in range(3,int(math.sqrt(n))+1,2):
-        if n % i == 0:
+
+    r, d = 0, n - 1
+    while d % 2 == 0:
+        r += 1
+        d //= 2
+
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            continue
+        for __ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
             return False
     return True
 
